d154c54436c985cb68b9e14050f3dd9e0dd661da
William <william.deazevedopro@gmail.com>
2023-07-26 14:57:18 +0200

    Font - Typo - Colors
    Setup de la typo - setup de toute les couleurs - setup des variantes de heading et p

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import Providers from "./Providers";
 import ThemeSwitcher from "./ThemeSwitcher";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Kanban",
@@ -20,7 +26,7 @@ export default function RootLayout({
       style={{ colorScheme: "light" }}
       suppressHydrationWarning
     >
-      <body>
+      <body className={jakarta.className}>
         <Providers>
           <ThemeSwitcher />
           {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-purple': '#635FC7',
+        'main-purple-hover': '#A8A4FF',
+        'main-black': '#000112',
+        'very-dark-grey': '#20212C',
+        'dark-grey': '#2B2C37',
+        'grey': '#3E3F4E',
+        'medium-grey': '#828FA3',
+        'light-grey': '#E4EBFA',
+        'very-light-grey': '#F4F7FD',
+        'main-red': '#EA5555',
+        'main-red-hover': '#FF9898',
+      }
+    },
   },
   plugins: [],
 };
